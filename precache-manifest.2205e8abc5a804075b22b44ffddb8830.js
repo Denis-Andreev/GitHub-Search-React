01154558abd7ca4264f035710f28f6301e280cde
Denis-Andreev <denisandreev5122@gmail.com>
2020-06-08 05:52:17 +0300

    adding build for demonstration

---
+++ precache-manifest.2205e8abc5a804075b22b44ffddb8830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e410ba40c1597b515acd633de32c651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "752135ebd5644af963dd",
+    "url": "/static/css/main.1a65e331.chunk.css"
+  },
+  {
+    "revision": "5292cd177ed14fd1e984",
+    "url": "/static/js/2.71fd4ac1.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.71fd4ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752135ebd5644af963dd",
+    "url": "/static/js/main.8ab845a4.chunk.js"
+  },
+  {
+    "revision": "75bf3345bb6ccc802852",
+    "url": "/static/js/runtime-main.9a655f22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
